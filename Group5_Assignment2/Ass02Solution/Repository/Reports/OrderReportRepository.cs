36cf8ec66450182802b508f7a24e2c90696588af
Nguyễn Trường Huy <dopi4720@gmail.com>
2024-01-30 14:37:35 +0700

    .

---
+++ Group5_Assignment2/Ass02Solution/Repository/Reports/OrderReportRepository.cs
@@ -0,0 +1,22 @@
+﻿using BusinessObject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Reports
+{
+    public class OrderReportRepository
+    {
+        public OrderReportRepository()
+        {
+            OrderDetails = new List<OrderDetail>();
+        }
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime ShippedDate { get; set; }
+        public int Revenue { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; }
+    }
+}
--- Group5_Assignment2/Ass02Solution/SalesRazorPagesApp/Pages/AdminPages/Reports/ReportManager.cshtml.cs
+++ Group5_Assignment2/Ass02Solution/SalesRazorPagesApp/Pages/AdminPages/Reports/ReportManager.cshtml.cs
@@ -1,12 +1,36 @@
+using BusinessObject.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Repository.Reports;
 
 namespace SalesRazorPagesApp.Pages.AdminPages.Reports
 {
     public class ReportManagerModel : PageModel
     {
-        public void OnGet()
+        public void OnGet(DateTime fromDate, DateTime toDate)
         {
+            using (var context = new eStoreContext())
+            {
+                var orderReports = context.Orders
+                    .Where(o => o.OrderDate >= fromDate && o.OrderDate <= toDate)
+                    .Select(o => new OrderReportRepository
+                    {
+                        OrderId = o.OrderId,
+                        OrderDate = o.OrderDate,
+                        ShippedDate = o.ShippedDate,
+                        Revenue = o.OrderDetails.Sum(od => (od.UnitPrice * od.Quantity) - od.Discount),
+                        OrderDetails = o.OrderDetails.Select(od => new OrderDetail
+                        {
+                            ProductId = od.ProductId,
+                            Quantity = od.Quantity,
+                            UnitPrice = od.UnitPrice,
+                            Discount = od.Discount
+                        }).ToList()
+                    })
+                    .ToList();
+
+                ViewData["report"]= orderReports;
+            }
         }
     }
 }
